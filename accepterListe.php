c7336f1c484c08d8fe729bbd6dd5a1b5e7caf744
Dam1Dam <damien.cantin@orange.fr>
2019-01-16 01:11:12 +0100

    Update accepterListe.php

--- accepterListe.php
+++ accepterListe.php
@@ -1,8 +1,43 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Vide Dressing</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/button_css.css" />
+		<link rel="icon" type="image/ico" href="images/favicon.ico" />
+
+	</head>
+
+
+
 <?php
 
 
 include 'connectBD.php';
 
+
+$req = $pdo->prepare("SELECT * FROM liste WHERE statut='soumis'");
+$req->execute();
+
+echo '<table>';
+
+while ($row = $req->fetch(PDO::FETCH_ASSOC)){
+
+	
+
+	echo '<tr><th>'.$row['codeListe'].'</th><th>'.$row['statut'].'</th><th>';
+
+	echo "<a href=accepterListe2.php?id=".$row['codeListe']."><input type=\"submit\" value=\"acceptee\" ></a>";
+
+	echo '</th></tr>';
+
+	
+
+}
+
+echo '</table>';
 
 
 
